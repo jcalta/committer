8e23fd377e9ed272a29dc1ad1b1980160c99044d
Michael Gruber <aelgru@gmail.com>
2012-07-20 17:23:52 +0200

    building egg file

--- build.py
+++ build.py
@@ -23,7 +23,6 @@ from pythonbuilder.core import Author, init, use_plugin
 use_plugin('python.core')
 use_plugin('python.coverage')
 use_plugin('python.distutils')
-use_plugin('python.integrationtest')
 use_plugin('python.pychecker')
 use_plugin('python.pylint')
 use_plugin('python.unittest')
@@ -32,11 +31,14 @@ authors = [Author('Michael Gruber', 'aelgru@gmail.com')]
 license = 'Apache License, Version 2.0'
 summary = 'committer - supports iterative and incremental work with repositories.'
 url     = 'https://github.com/aelgru/committer'
-version = '0.0.15'
+version = '0.0.16'
 
-default_task = ['analyze', 'run_integration_tests']
+default_task = ['analyze', 'publish']
 
 @init
 def set_properties (project):
     project.set_property('coverage_break_build', False)
     project.set_property('pychecker_break_build', True)
+
+    project.get_property('distutils_commands').append('bdist_egg')
+

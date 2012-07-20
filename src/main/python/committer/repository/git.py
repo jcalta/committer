f6af40645f43938117fde99cda780a0577b27770
Michael Gruber <aelgru@gmail.com>
2012-07-20 18:48:40 +0200

    git module now detects if current directory is a repository

--- src/main/python/committer/repository/git.py
+++ src/main/python/committer/repository/git.py
@@ -1,11 +1,25 @@
-#   Copyright 2012, Michael Gruber
-#   Licensed under Apache License, Version 2.0
+#   git wrapper module for committer
+#   Copyright 2012 Michael Gruber
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 """
     Git wrapper module.
 """
 
 import subprocess
+
+from os import path
 
 
 def commit (message):
@@ -15,6 +29,16 @@ def commit (message):
     
     _git('commit', '-a', '-m', message)
 
+
+def detect ():
+    """
+        returns True if the current directory represents a git repository,
+        otherwise it will return False.
+    """
+    
+    return path.isdir('.git')
+
+
 def pull ():
     """
         pulls from repository.
@@ -22,12 +46,14 @@ def pull ():
     
     _git('pull')
 
+
 def push ():
     """
         pushes to repository.
     """
     
     _git('push')
+
 
 def _git (*args):
     """
@@ -37,6 +63,7 @@ def _git (*args):
     arguments = list(args)
     arguments.insert(0, 'git')
     subprocess.call(arguments)
+
 
 def _ensure_git_is_executable ():
     """
--- src/unittest/python/git_tests.py
+++ src/unittest/python/git_tests.py
@@ -1,48 +1,63 @@
 import unittest
 
-from mock import (call,
-                  patch,
-                  Mock)
+from mock import Mock, call, patch
 
 from committer.repository import git
 
 
 class GitTests (unittest.TestCase):
     @patch('committer.repository.git.subprocess')        
-    def test_should_call_git_in_subprocess (self, subprocess_mock):
+    def test_should_call_git_in_subprocess (self, mock_subprocess):
         git._git()
         
-        self.assertEquals(call(['git']), subprocess_mock.call.call_args)
+        self.assertEquals(call(['git']), mock_subprocess.call.call_args)
 
     @patch('committer.repository.git.subprocess')        
-    def test_should_call_git_using_given_arguments (self, subprocess_mock):
+    def test_should_call_git_using_given_arguments (self, mock_subprocess):
         git._git('1', '2', '3')
         args = (['git', '1', '2', '3'])
-        self.assertEquals(call(args), subprocess_mock.call.call_args)
+        self.assertEquals(call(args), mock_subprocess.call.call_args)
         
     @patch('committer.repository.git._git')
-    def test_should_prepend_git_to_given_arguments (self, git_mock):
+    def test_should_prepend_git_to_given_arguments (self, mock_git):
         
         git.commit('This is a commit message.')
         
-        self.assertEquals( \
-                    call('commit', '-a', '-m', 'This is a commit message.'), \
-                    git_mock.call_args)
+        self.assertEquals(call('commit', '-a', '-m', 'This is a commit message.'), mock_git.call_args)
 
     @patch('committer.repository.git._git')
-    def test_should_call_git_pull (self, git_mock):
+    def test_should_call_git_pull (self, mock_git):
         git.pull()
         
-        self.assertEquals(call('pull'), git_mock.call_args)
+        self.assertEquals(call('pull'), mock_git.call_args)
 
     @patch('committer.repository.git._git')
-    def test_should_call_git_push (self, git_mock):
+    def test_should_call_git_push (self, mock_git):
         git.push()
         
-        self.assertEquals(call('push'), git_mock.call_args)
+        self.assertEquals(call('push'), mock_git.call_args)
 
     @patch('committer.repository.git.subprocess')        
-    def test_should_execute_check_call_on_git_version (self, subprocess_mock):
+    def test_should_execute_check_call_on_git_version (self, mock_subprocess):
         git._ensure_git_is_executable()
         
-        self.assertEquals(call(['git', '--version']), subprocess_mock.check_call.call_args)
+        self.assertEquals(call(['git', '--version']), mock_subprocess.check_call.call_args)
+
+    @patch('os.path.isdir')
+    def test_return_false_if_dot_git_directory_does_not_exist (self, mock_exists):
+        mock_exists.return_value = False
+        
+        actual_return_value = git.detect()
+        
+        self.assertEquals(False, actual_return_value)
+        self.assertEquals(call('.git'), mock_exists.call_args)
+        
+    @patch('os.path.isdir')
+    def test_return_true_if_dot_git_directory_exists (self, mock_exists):
+        mock_exists.return_value = True
+        
+        actual_return_value = git.detect()
+        
+        self.assertEquals(True, actual_return_value)
+        self.assertEquals(call('.git'), mock_exists.call_args)
+        

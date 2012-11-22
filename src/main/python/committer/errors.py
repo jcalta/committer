f9ecc564aaacf386bc28b340c8e1de063da6a835
Michael Gruber <aelgru@gmail.com>
2012-11-22 21:38:36 +0100

    fixed wrong usage information

--- src/main/python/committer/errors.py
+++ src/main/python/committer/errors.py
@@ -61,9 +61,9 @@ class WrongUsageException (CommitterException):
 Wrong usage, please use the committer commands as expected.
 
 usage:
-    commit "message" [++]    commits all changes
-    st                       shows all changes
-    update                   updates the current directory
+    co "message" [++]    commits all changes
+    st                   shows all changes
+    up                   updates the current directory
 """, 1)
 
 

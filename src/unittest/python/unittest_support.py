061b99a681338defbd6221dce515a7c8dd9595c8
aelgru <aelgru@gmail.com>
2012-12-04 02:23:04 +0100

    Uncle Bob is right: this doesn't really help.

--- src/unittest/python/unittest_support.py
+++ src/unittest/python/unittest_support.py
@@ -5,7 +5,7 @@ from mock import Mock
 
 class TestCase (unittest.TestCase):
     def create_mock_vcs_client (self):
-        mock_vcs_client         = Mock()
+        mock_vcs_client = Mock()
         mock_vcs_client.COMMAND = 'mock-vcs-command'
-        mock_vcs_client.NAME    = 'MockRepository'
+        mock_vcs_client.NAME = 'MockRepository'
         return mock_vcs_client    

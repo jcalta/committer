412b02bf6513914b8e8e446f09c7b2b66a1305c6
aelgru <aelgru@gmail.com>
2012-12-07 17:32:10 +0100

    using mockito

--- src/unittest/python/vcsclients/util_tests.py
+++ src/unittest/python/vcsclients/util_tests.py
@@ -1,59 +1,58 @@
 import unittest
 import subprocess
 
-from mock import call, patch
+from mockito import when, verify, unstub, any as any_value
 
 from committer.vcsclients.util import VcsClient
 
+import committer
 
-class ExecuteCommandTests (unittest.TestCase):
+class VcsClientTests (unittest.TestCase):
     def setUp(self):
         self.vcs_client = VcsClient()
         
-    @patch('committer.vcsclients.util.call')        
-    def test_should_call_command_in_subprocess (self, mock_call):
+    def tearDown(self):
+        unstub()
+        
+    def test_should_call_command_in_subprocess (self):
+        when(committer.vcsclients.util).call(any_value()).thenReturn(None)
+        
         self.vcs_client.execute_command('command')
         
-        self.assertEqual(call(['command']), mock_call.call_args)
+        verify(committer.vcsclients.util).call(['command'])
 
-    @patch('committer.vcsclients.util.call')        
-    def test_should_call_command_using_given_arguments (self, mock_call):
+    def test_should_call_command_using_given_arguments (self):
+        when(committer.vcsclients.util).call(any_value(), any_value(), any_value(), any_value()).thenReturn(None)
+
         self.vcs_client.execute_command('command', '1', '2', '3')
         
-        self.assertEqual(call(['command', '1', '2', '3']), mock_call.call_args)
+        verify(committer.vcsclients.util).call(['command', '1', '2', '3'])
 
+    def test_should_return_true_when_command_is_executable (self):
+        when(committer.vcsclients.util).check_call(any_value(), any_value(), any_value()).thenReturn(None)
         
-class CheckIfIsExecutableTests (unittest.TestCase):
-    def setUp(self):
-        self.vcs_client = VcsClient()
-        
-    @patch('committer.vcsclients.util.check_call')        
-    def test_should_return_true_when_command_is_executable (self, mock_check_call):
         actual_result = self.vcs_client.check_if_is_executable('command', '--version', '--quiet')
         
         self.assertTrue(actual_result)
-        self.assertEqual(call(['command', '--version', '--quiet']), mock_check_call.call_args)
+        verify(committer.vcsclients.util).check_call(['command', '--version', '--quiet'])
 
-    @patch('committer.vcsclients.util.check_call')        
-    def test_should_return_false_when_command_is_not_executable (self, mock_check_call):
-        mock_check_call.side_effect = subprocess.CalledProcessError(127, 'command')
+    def test_should_return_false_when_command_is_not_executable (self):
+        when(committer.vcsclients.util).check_call(any_value()).thenRaise(subprocess.CalledProcessError(127, 'command'))
         
         actual_result = self.vcs_client.check_if_is_executable('command', '--version', '--quiet')
         
         self.assertFalse(actual_result)
-        self.assertEqual(call(['command', '--version', '--quiet']), mock_check_call.call_args)
+        verify(committer.vcsclients.util).check_call(['command', '--version', '--quiet'])
 
-    @patch('committer.vcsclients.util.check_call')        
-    def test_should_return_false_when_trying_to_execute_command_fails (self, mock_check_call):
-        mock_check_call.side_effect = OSError()
+    def test_should_return_false_when_trying_to_execute_command_fails (self):
+        when(committer.vcsclients.util).check_call(any_value()).thenRaise(OSError())
         
         actual_result = self.vcs_client.check_if_is_executable('command', '--version', '--quiet')
         
         self.assertFalse(actual_result)
-        self.assertEqual(call(['command', '--version', '--quiet']), mock_check_call.call_args)
+        verify(committer.vcsclients.util).check_call(['command', '--version', '--quiet'])
 
-    @patch('committer.vcsclients.util.check_call')        
-    def test_should_raise_eception_when_during_check_something_unexpected_happens (self, mock_check_call):
-        mock_check_call.side_effect = Exception('Not executable')
+    def test_should_raise_eception_when_during_check_something_unexpected_happens (self):
+        when(committer.vcsclients.util).check_call(any_value()).thenRaise(Exception('Not executable'))
         
-        self.assertRaises(Exception, self.vcs_client.check_if_is_executable, ('command', '--version', '--quiet'))
+        self.assertRaises(Exception, self.vcs_client.check_if_is_executable, (['command', '--version', '--quiet']))

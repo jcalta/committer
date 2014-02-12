692cda821090286309887b7570cc3621d4fc400c
aelgru <aelgru@gmail.com>
2014-02-12 21:47:39 +0100

    Moving initialization of process mock into setup

--- src/unittest/python/vcsclients/execution_tests.py
+++ src/unittest/python/vcsclients/execution_tests.py
@@ -29,35 +29,33 @@ class ExecuteCommandTests(UnitTests):
         when(execution.LOGGER).info(ANY_ARGUMENTS).then_return(None)
         when(execution.LOGGER).error(ANY_ARGUMENTS).then_return(None)
 
-    def test_should_call_command_in_subprocess(self):
         process_mock = Mock()
         process_mock.returncode = 0
-        when(process_mock).communicate().then_return(('stdout', 'stderr'))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        self.process_mock = process_mock
+
+    def test_should_call_command_in_subprocess(self):
+        when(self.process_mock).communicate().then_return(('stdout', 'stderr'))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         execute_command('command')
 
         verify(execution).Popen(['command'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        verify(process_mock).communicate()
+        verify(self.process_mock).communicate()
 
     def test_should_call_command_using_given_arguments(self):
-        process_mock = Mock()
-        process_mock.returncode = 0
-        when(process_mock).communicate().then_return(('stdout', 'stderr'))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        when(self.process_mock).communicate().then_return(('stdout', 'stderr'))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         execute_command('command', '1', '2', '3')
 
         verify(execution).Popen(['command', '1', '2', '3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        verify(process_mock).communicate()
+        verify(self.process_mock).communicate()
 
     def test_should_return_stdout_and_stderr_and_returncode_when_executing_command(self):
         stdout = 'stdout'
         stderr = 'stderr'
-        process_mock = Mock()
-        process_mock.returncode = 0
-        when(process_mock).communicate().then_return((stdout, stderr))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        when(self.process_mock).communicate().then_return((stdout, stderr))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         actual = execute_command('command', '1', '2', '3')
 
@@ -68,10 +66,8 @@ class ExecuteCommandTests(UnitTests):
     def test_should_log_stdout_when_stdout_is_not_empty_string(self):
         stdout = 'stdout'
         stderr = 'stderr'
-        process_mock = Mock()
-        process_mock.returncode = 0
-        when(process_mock).communicate().then_return((stdout, stderr))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        when(self.process_mock).communicate().then_return((stdout, stderr))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         execute_command('command', '1', '2', '3')
 
@@ -80,10 +76,8 @@ class ExecuteCommandTests(UnitTests):
     def test_should_not_log_stdout_when_stdout_is_empty_string(self):
         stdout = ''
         stderr = 'stderr'
-        process_mock = Mock()
-        process_mock.returncode = 0
-        when(process_mock).communicate().then_return((stdout, stderr))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        when(self.process_mock).communicate().then_return((stdout, stderr))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         execute_command('command', '1', '2', '3')
 
@@ -92,10 +86,8 @@ class ExecuteCommandTests(UnitTests):
     def test_should_log_stderr_when_stderr_is_not_empty_string(self):
         stdout = 'stdout'
         stderr = 'stderr'
-        process_mock = Mock()
-        process_mock.returncode = 0
-        when(process_mock).communicate().then_return((stdout, stderr))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        when(self.process_mock).communicate().then_return((stdout, stderr))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         execute_command('command', '1', '2', '3')
 
@@ -105,10 +97,8 @@ class ExecuteCommandTests(UnitTests):
 
         stdout = 'stdout'
         stderr = ''
-        process_mock = Mock()
-        process_mock.returncode = 0
-        when(process_mock).communicate().then_return((stdout, stderr))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        when(self.process_mock).communicate().then_return((stdout, stderr))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
 
         execute_command('command', '1', '2', '3')
 
@@ -118,10 +108,9 @@ class ExecuteCommandTests(UnitTests):
 
         stdout = 'stdout'
         stderr = ''
-        process_mock = Mock()
-        process_mock.returncode = 123
-        when(process_mock).communicate().then_return((stdout, stderr))
-        when(execution).Popen(ANY_ARGUMENTS).then_return(process_mock)
+        self.process_mock.returncode = 123
+        when(self.process_mock).communicate().then_return((stdout, stderr))
+        when(execution).Popen(ANY_ARGUMENTS).then_return(self.process_mock)
         when(execution).exit(ANY_ARGUMENTS).then_return(None)
 
         execute_command('command', '1', '2', '3')
@@ -131,9 +120,8 @@ class ExecuteCommandTests(UnitTests):
     def test_should_catch_os_error_and_exit_when_popen_raises_exception(self):
         stdout = 'stdout'
         stderr = ''
-        process_mock = Mock()
-        process_mock.returncode = 123
-        when(process_mock).communicate().then_return((stdout, stderr))
+        self.process_mock.returncode = 123
+        when(self.process_mock).communicate().then_return((stdout, stderr))
         when(execution).Popen(ANY_ARGUMENTS).then_raise(OSError("[Errno 2] No such file or directory"))
         when(execution).exit(ANY_ARGUMENTS).then_return(None)
 
